ccc01c3dc968a6e50965597725246fd423579289
sinapad <malinoski.iuri@gmail.com>
2019-11-16 16:34:22 -0200

    Add giraffe exercice

---
+++ src/am2018/GiraffeZoo.java
@@ -0,0 +1,16 @@
+package am2018;
+
+public class GiraffeZoo{
+    public static void main(String ars[]) {
+
+        GiraffeZoo exercice = new GiraffeZoo();
+        int [] A = new int [] {1,5,4,9,8,7,12,13,14};
+        // int [] A = new int [] {4,3,2,6,1};
+        int result = exercice.solution(A);
+        System.out.println("RESULT: "+result);        
+    }
+
+    public int solution (int [] A){
+        return 0;
+    }
+}
